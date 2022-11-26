b49a1aebc2228134aaea916fc306d9cb958aa591
unknown <mwgoodwin2000@gmail.com>
2022-11-26 02:00:57 -0700

    bug fixes

--- script.js
+++ script.js
@@ -23,25 +23,25 @@ function renderedInput() {
     for (var i = 0; i < hours.length; i++) {
         var hourlyDiv = $('<div>').addClass('row');
         containerEl.append(hourlyDiv);
- 
-        const currentDate =  moment(hours[i], 'H')
+
+        const currentDate = moment(hours[i], 'H')
 
         const formattedHour = currentDate.format('hha')
 
-        var hourEl = $('<span>').text(formattedHour).addClass('col-1 text-center hour pt-2').data({hours: [i]});
+        var hourEl = $('<span>').text(formattedHour).addClass('col-1 text-center hour pt-2').data({ hours: [i] });
         hourlyDiv.append(hourEl);
 
-        var textInput = $('<input>', {type: 'text', id: i, placeholder: 'write daily task here'}).addClass('col-10 textarea');
+        var textInput = $('<input>', { type: 'text', id: i, placeholder: 'write daily task here' }).addClass('col-10 textarea');
         hourlyDiv.append(textInput);
 
-        var saveBtn = $('<button>', {id: i}).text('Save').addClass('col-1 btn saveBtn').attr('data-uid', i);
+        var saveBtn = $('<button>', { id: i }).text('Save').addClass('col-1 btn saveBtn').attr('data-uid', i);
         hourlyDiv.append(saveBtn);
 
         presentColor(i);
     }
 
-    const localStorageData =  JSON.parse(localStorage.getItem('tasks'));
-    if(localStorageData){
+    const localStorageData = JSON.parse(localStorage.getItem('tasks'));
+    if (localStorageData) {
         saveData = localStorageData;
     }
 
@@ -50,13 +50,12 @@ function renderedInput() {
         $(`input[id=${index}]`).val(textData);
     })
 
-    $('button').on('click', function(event){
+    $('button').on('click', function (event) {
         const value = $(event.target).siblings('input').val();
         const index = parseInt($(event.target).attr('data-uid'));
         saveData[index] = value;
         saveToLocalStorage();
     });
-
 }
 
 function presentColor(i) {
@@ -64,14 +63,14 @@ function presentColor(i) {
     console.log(currentHour)
     if (currentHour == hours[i]) {
         $(`input[id=${i}]`).addClass('present')
-    } else if (currentHour < hours[i]) { 
+    } else if (currentHour < hours[i]) {
         $(`input[id=${i}]`).addClass('future')
     } else {
         $(`input[id=${i}]`).addClass('past')
     }
 }
 function saveToLocalStorage() {
-   localStorage.setItem('tasks', JSON.stringify(saveData));
+    localStorage.setItem('tasks', JSON.stringify(saveData));
 }
 
 renderedInput();
